d1624c26fbff0ac1c4dfdd48705c93c7e66b3848
Nhat Vo <nhat@sensennetworks.com>
2024-09-24 06:35:01 +1000

    .

--- app/agent/urls.py
+++ app/agent/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import WixProductListView
 
 urlpatterns = [
-    path('wix-products/', WixProductListView.as_view(), name='wix_product_list'),  # URL to view WixProducts and export CSV
+    path('wix-products/', WixProductListView.as_view(), name='wix_product_list'),  # This must match exactly
 ]
