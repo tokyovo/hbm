c942921d26e2bfdd430fd94e88a168cd172ae5e9
Nhat Vo <nhat@sensennetworks.com>
2024-09-24 05:20:52 +1000

    .

--- app/agent/admin.py
+++ app/agent/admin.py
@@ -146,7 +146,7 @@ class WixProductAdmin(admin.ModelAdmin):
     )
 
     def get_collections(self, obj):
-        collections = obj.collection.all()
+        collections = obj.collection
         links = [format_html('<a href="{}">{}</a>', reverse('admin:agent_collection_change', args=[c.id]), c.title) for c in collections]
         return format_html(", ".join(links))
 
