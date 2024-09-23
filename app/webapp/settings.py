6a415551863e40463214ca656f8f9c0c1733a5e7
Nhat Vo <nhat@sensennetworks.com>
2024-09-24 09:31:57 +1000

    .

--- app/webapp/settings.py
+++ app/webapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = ['seoai.japanvo.bid', 'localhost', '35.209.48.62']  # Update this with your actual domain
+ALLOWED_HOSTS = ['*']  # Update this with your actual domain
 CSRF_TRUSTED_ORIGINS = ['https://seoai.japanvo.bid', 'http://localhost']
 
 # Application definition
